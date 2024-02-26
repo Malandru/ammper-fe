b17e49b3c8c60df1d0a8da8f2063bb506b773d58
Andres Martinez <andre.mtzd07@gmail.com>
2024-02-26 16:28:55 -0600

    same instance

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,3 @@
 export default class URLS {
-    static AMMPER_BASE_URL = 'http://ec2-3-22-138-191.us-east-2.compute.amazonaws.com';
+    static AMMPER_BASE_URL = 'http://ec2-3-22-138-191.us-east-2.compute.amazonaws.com:8000';
 }
